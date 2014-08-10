4e7194a39aae277042121ab383880d38f861e79a
Phil Cockfield <phil@cockfield.net>
2014-08-09 20:18:39 -0700

    Update refs to css-stylus.

--- package.js
+++ package.js
@@ -7,7 +7,7 @@ Package.describe({
 Package.on_use(function (api) {
   api.use(['coffeescript', 'sugar', 'http']);
   api.use(['templating'], 'client');
-  api.use(['ctrl', 'util', 'stylus-compiler']);
+  api.use(['ctrl', 'util', 'css-stylus']);
 
   // Generated with: github.com/philcockfield/meteor-package-paths
   api.add_files('client/checkbox-slider/checkbox-slider.html', 'client');
