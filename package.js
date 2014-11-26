15fe810187c55ceff48e3151e5dca7d6dc9c0a64
Phil Cockfield <phil@cockfield.net>
2014-11-26 11:46:49 -0800

    Update [package.on_use] => [package.onUse]

--- package.js
+++ package.js
@@ -7,56 +7,56 @@ Package.describe({
 
 
 
-Package.on_use(function (api) {
+Package.onUse(function (api) {
   api.use(['coffeescript', 'http']);
   api.use(['templating', 'ui', 'spacebars'], 'client');
   api.use(['respondly:ctrl', 'respondly:util', 'respondly:css-stylus']);
   api.export('Ctrls');
 
   // Generated with: github.com/philcockfield/meteor-package-paths
-  api.add_files('shared/ns.js', ['client', 'server']);
-  api.add_files('shared/css-mixins/textbox.import.styl', 'client');
-  api.add_files('shared/css-mixins/textbox.import.styl', 'server', { isAsset:true });
-  api.add_files('shared/util.coffee', ['client', 'server']);
-  api.add_files('client/textboxes/content-editable/ctrl/content-editable.html', 'client');
-  api.add_files('client/textboxes/raw-textbox/raw-textbox.html', 'client');
-  api.add_files('client/textboxes/text-input/text-input.html', 'client');
-  api.add_files('client/button/button.html', 'client');
-  api.add_files('client/checkbox/checkbox.html', 'client');
-  api.add_files('client/checkbox-tree/checkbox-tree.html', 'client');
-  api.add_files('client/radio/radio.html', 'client');
-  api.add_files('client/radios/radios.html', 'client');
-  api.add_files('client/select/select.html', 'client');
-  api.add_files('client/textboxes/content-editable/lib/css/medium-editor.css', 'client');
-  api.add_files('client/textboxes/content-editable/lib/css/theme-default.css', 'client');
-  api.add_files('client/textboxes/content-editable/lib/js/medium-editor.js', 'client');
-  api.add_files('client/textboxes/content-editable/ctrl/content-editable.coffee', 'client');
-  api.add_files('client/textboxes/content-editable/ctrl/content-editable.styl', 'client');
-  api.add_files('client/textboxes/validation/email_validator.coffee', 'client');
-  api.add_files('client/textboxes/validation/screen_name_validator.coffee', 'client');
-  api.add_files('client/textboxes/content-editable/content-editable-controller.coffee', 'client');
-  api.add_files('client/textboxes/content-editable/css.styl', 'client');
-  api.add_files('client/textboxes/raw-textbox/raw-textbox.coffee', 'client');
-  api.add_files('client/textboxes/raw-textbox/raw-textbox.styl', 'client');
-  api.add_files('client/textboxes/text-input/text-input.coffee', 'client');
-  api.add_files('client/textboxes/text-input/text-input.styl', 'client');
-  api.add_files('client/button/button.coffee', 'client');
-  api.add_files('client/button/button.styl', 'client');
-  api.add_files('client/textboxes/textbox-date.coffee', 'client');
-  api.add_files('client/checkbox/checkbox.coffee', 'client');
-  api.add_files('client/checkbox/checkbox.styl', 'client');
-  api.add_files('client/checkbox-tree/checkbox-tree.coffee', 'client');
-  api.add_files('client/checkbox-tree/checkbox-tree.styl', 'client');
-  api.add_files('client/radio/radio.coffee', 'client');
-  api.add_files('client/radio/radio.styl', 'client');
-  api.add_files('client/radios/radios.coffee', 'client');
-  api.add_files('client/radios/radios.styl', 'client');
-  api.add_files('client/select/select.coffee', 'client');
-  api.add_files('client/select/select.styl', 'client');
-  api.add_files('client/common.styl', 'client');
-  api.add_files('client/data-binder.coffee', 'client');
-  api.add_files('images/text_input_error.svg', ['client', 'server']);
-  api.add_files('images/text_input_tick.svg', ['client', 'server']);
+  api.addFiles('shared/ns.js', ['client', 'server']);
+  api.addFiles('shared/css-mixins/textbox.import.styl', 'client');
+  api.addFiles('shared/css-mixins/textbox.import.styl', 'server', { isAsset:true });
+  api.addFiles('shared/util.coffee', ['client', 'server']);
+  api.addFiles('client/textboxes/content-editable/ctrl/content-editable.html', 'client');
+  api.addFiles('client/textboxes/raw-textbox/raw-textbox.html', 'client');
+  api.addFiles('client/textboxes/text-input/text-input.html', 'client');
+  api.addFiles('client/button/button.html', 'client');
+  api.addFiles('client/checkbox/checkbox.html', 'client');
+  api.addFiles('client/checkbox-tree/checkbox-tree.html', 'client');
+  api.addFiles('client/radio/radio.html', 'client');
+  api.addFiles('client/radios/radios.html', 'client');
+  api.addFiles('client/select/select.html', 'client');
+  api.addFiles('client/textboxes/content-editable/lib/css/medium-editor.css', 'client');
+  api.addFiles('client/textboxes/content-editable/lib/css/theme-default.css', 'client');
+  api.addFiles('client/textboxes/content-editable/lib/js/medium-editor.js', 'client');
+  api.addFiles('client/textboxes/content-editable/ctrl/content-editable.coffee', 'client');
+  api.addFiles('client/textboxes/content-editable/ctrl/content-editable.styl', 'client');
+  api.addFiles('client/textboxes/validation/email_validator.coffee', 'client');
+  api.addFiles('client/textboxes/validation/screen_name_validator.coffee', 'client');
+  api.addFiles('client/textboxes/content-editable/content-editable-controller.coffee', 'client');
+  api.addFiles('client/textboxes/content-editable/css.styl', 'client');
+  api.addFiles('client/textboxes/raw-textbox/raw-textbox.coffee', 'client');
+  api.addFiles('client/textboxes/raw-textbox/raw-textbox.styl', 'client');
+  api.addFiles('client/textboxes/text-input/text-input.coffee', 'client');
+  api.addFiles('client/textboxes/text-input/text-input.styl', 'client');
+  api.addFiles('client/button/button.coffee', 'client');
+  api.addFiles('client/button/button.styl', 'client');
+  api.addFiles('client/textboxes/textbox-date.coffee', 'client');
+  api.addFiles('client/checkbox/checkbox.coffee', 'client');
+  api.addFiles('client/checkbox/checkbox.styl', 'client');
+  api.addFiles('client/checkbox-tree/checkbox-tree.coffee', 'client');
+  api.addFiles('client/checkbox-tree/checkbox-tree.styl', 'client');
+  api.addFiles('client/radio/radio.coffee', 'client');
+  api.addFiles('client/radio/radio.styl', 'client');
+  api.addFiles('client/radios/radios.coffee', 'client');
+  api.addFiles('client/radios/radios.styl', 'client');
+  api.addFiles('client/select/select.coffee', 'client');
+  api.addFiles('client/select/select.styl', 'client');
+  api.addFiles('client/common.styl', 'client');
+  api.addFiles('client/data-binder.coffee', 'client');
+  api.addFiles('images/text_input_error.svg', ['client', 'server']);
+  api.addFiles('images/text_input_tick.svg', ['client', 'server']);
 
 });
 
