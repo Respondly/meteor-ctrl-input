64a170efea1c7472e01c03850adf17a798939862
Mike Risse <rissem@gmail.com>
2015-01-21 09:57:24 -0800

    fix broken package.js file

--- package.js
+++ package.js
@@ -73,8 +73,6 @@ Package.onUse(function (api) {
 });
 
 Package.onTest(function(api){
-
-Package.onTest(function(api){
   api.use(['mike:mocha-package@0.4.7', 'coffeescript']);
 
   // Generated with: github.com/philcockfield/meteor-package-paths
